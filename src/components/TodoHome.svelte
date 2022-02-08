e1edf34d5bef38f0f5ef39be9a41b19d6a442e35
Best-Verie <irabestverie2016@gmail.com>
2022-02-08 13:37:34 +0200

    added single item grid

--- src/components/TodoHome.svelte
+++ src/components/TodoHome.svelte
@@ -11,3 +11,17 @@
     <TodoItem />
   </div>
 </div>
+
+<style>
+  .left-item {
+    grid-area: left_item;
+  }
+  .right_item {
+    grid-area: right_item;
+  }
+  .grid-container {
+    display: grid;
+    grid-gap: 10px;
+    grid-template-areas: "left_item left_item right_item right_item right_item right_item";
+  }
+</style>
--- src/components/TodoItem.svelte
+++ src/components/TodoItem.svelte
@@ -1,2 +1,15 @@
 <script>
 </script>
+
+<div class="item">
+  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore totam
+  impedit earum natus ratione praesentium distinctio numquam autem culpa odio.
+  Dolorum iste assumenda tempore officia nesciunt nobis? Eveniet, rerum
+  pariatur!
+</div>
+
+<style>
+  .item {
+    margin-top: 8em;
+  }
+</style>
